f402dcabd9c2d6013a2fb112008891d6f33f5a9d
Justice <justice@Justices-MacBook-Pro.local>
2020-03-13 02:05:16 -0500

    deleted old terraform state files

--- stage/services/webserver-cluster/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-	backend "s3" {
-		encrypt		= true
-		bucket		= "terraform-up-and-running-state-cj-1"
-		dynamodb_table	= "terraform-state-lock-dynamo"
-		region		= "us-east-1"
-		key 		= "stage/services/webserver-cluster/terraform.tfstate"
-	}
-}
--- state_lock.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-up-and-running-state-cj-1"
-    key     = "global/s3/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = "true"
-  }
-}
-
